f5caa5fa6a1019f0bae2cd55458c4f8d1f5e732e
ivana-fimic <ek.ivanafimic@gmail.com>
2024-05-26 15:47:07 +0200

    dodat login i register

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:20',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8|confirmed',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], Response::HTTP_BAD_REQUEST);
+        }
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return response()->json([
+            'user' => $user,
+            'message' => 'User Created',
+        ], Response::HTTP_CREATED);
+    }
+
+    public function login(Request $request)
+    {
+        $loginUserData = $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|min:8'
+        ]);
+        $user = User::where('email', $loginUserData['email'])->first();
+        if (!$user || !Hash::check($loginUserData['password'], $user->password)) {
+            return response()->json([
+                'message' => 'Invalid Credentials'
+            ], 401);
+        }
+        $token = $user->createToken($user->name . '-AuthToken')->plainTextToken;
+        return response()->json([
+            'user' => $user,
+            'access_token' => $token,
+        ]);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,19 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\RoomTypeController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::get('/room-types', [RoomTypeController::class, 'index'])->middleware('auth:sanctum');
+Route::get('/room-types/{id}', [RoomTypeController::class, 'show'])->middleware('auth:sanctum');
+Route::post('/room-types', [RoomTypeController::class, 'store'])->middleware('auth:sanctum');
+Route::put('/room-types/{id}', [RoomTypeController::class, 'update'])->middleware('auth:sanctum');
+Route::delete('/room-types/{id}', [RoomTypeController::class, 'destroy'])->middleware('auth:sanctum');
