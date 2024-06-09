68376b63c28d2c7bb4908f754874e1b3a95e402d
dimi20200005 <ds20200005@student.fon.bg.ac.rs>
2024-06-09 18:18:24 +0200

    implementirana login strana

--- reservation_frontend/src/App.js
+++ reservation_frontend/src/App.js
@@ -1,14 +1,19 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
+import { UserProvider } from "./context/UserContext";
+import Home from "./pages/Home";
 
 function App() {
 	return (
-		<div className="appDiv">
-			<Routes>
-				<Route path="/" element={<Login />} />
-			</Routes>
-		</div>
+		<UserProvider>
+			<div className="appDiv">
+				<Routes>
+					<Route path="/" element={<Login />} />
+					<Route path="/home" element={<Home />} />
+				</Routes>
+			</div>
+		</UserProvider>
 	);
 }
 
